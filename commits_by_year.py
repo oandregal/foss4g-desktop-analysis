d06a8c25f895ecefb2f31547c346fd29ce42a846
Andrés Maneiro <andres.maneiro@gmail.com>
2011-02-26 21:11:26 +0100

    added script to get commits by year

---
+++ commits_by_year.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import sys
+from pygooglechart import GroupedVerticalBarChart, Axis
+
+datafile = sys.argv[1]
+
+commitsfile = open(datafile, 'r')
+data_array = []
+legend_array = []
+
+chart = GroupedVerticalBarChart(500, 200,
+                                y_range=(0, 10000))
+
+chart.set_bar_width(50)
+
+for line in commitsfile:
+    (legend, data) = line.split(' ')
+    data_array.append(int(data.strip()))
+    legend_array.append(legend)
+
+print data_array
+chart.add_data(data_array)
+chart.set_axis_labels('x', legend_array)
+chart.set_axis_labels('y', ['0', '1000','2000','3000','4000','5000', '6000', '7000', '8000', '9000', '10000'])
+
+print chart.get_url()
+
+chart.download('images/commits_by_year_'+str(datafile.split('.')[0])+'.png')
+
