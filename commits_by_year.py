2e2413c8c24956f350c8175a029939f234d56fe5
Andrés Maneiro <andres.maneiro@gmail.com>
2011-03-01 23:39:37 +0100

    Added title to commits/year graph and reduce width

--- commits_by_year.py
+++ commits_by_year.py
@@ -6,10 +6,12 @@ from pygooglechart import GroupedVerticalBarChart, Axis
 
 datafile = sys.argv[1]
 commitsfile = open(datafile, 'r')
+name = os.path.basename(datafile)
+
 data_array = []
 legend_array = []
 
-chart = GroupedVerticalBarChart(500, 200,
+chart = GroupedVerticalBarChart(420, 200,
                                 y_range=(0, 5500))
 
 #chart.set_bar_width(25)
@@ -27,8 +29,9 @@ chart.add_data(data_array)
 chart.set_axis_labels('x', legend_array)
 chart.set_axis_labels('y', range(0,6000,500))
 
+chart.set_title(name.split('.')[0].upper()+" - # of commits/year")
+
 print chart.get_url()
 
-name = os.path.basename(datafile)
 chart.download('images/commits_by_year_'+str(name.split('.')[0])+'.png')
 
